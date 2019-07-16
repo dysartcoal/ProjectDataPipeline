63ab15361b240cbcf51ebf904c82790d7df39469
Kim Littlejohn <jkldatascience@gmail.com>
2019-07-16 15:58:32 +0100

    Simplify logic so that any test can be passed in as a parameter
    All logic has been removed so that any sql code can be passed
    in to be run as a test case and any assertion can be carried
    out on the results.

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -9,55 +9,27 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                 redshift_conn_id="",
-                table="",
-                primary_key="",
-                not_nulls=None,
+                tests=None,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.table = table
-        self.primary_key = primary_key
-        self.not_nulls = not_nulls
+        self.tests=tests
 
     def execute(self, context):
-        self.log.info(f'DataQualityOperator table: {self.table}')
+        self.log.info(f'DataQualityOperator')
         redshift_hook = PostgresHook(self.redshift_conn_id)
 
-        # Check that data exists
-        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
-        if len(records) < 1 or len(records[0]) < 1:
-            raise ValueError("Data quality check failed. {} returned no results".format(self.table))
-        num_records = records[0][0]
-        if num_records < 1:
-            raise ValueError("Data quality check failed. {} contained 0 rows".format(self.table))
-        self.log.info(f"Data quality check for data on table {self.table} passed with {records[0][0]} records")
+        for test in self.tests:
+            sql = test['sql']
+            assertion = test['assertion']
+            error_message = test['error_message']
+            log_message = test['log_message']
 
-        # Check that primary_key is unique
-        if self.primary_key != "":
-            check_primary_key_sql = """ SELECT COUNT(*) FROM
-                    (SELECT {}, COUNT({}) AS pkey_count
-                    FROM {}
-                    GROUP BY {}
-                    HAVING pkey_count > 1) dup_pkeys
-                    """
-            records = redshift_hook.get_records(check_primary_key_sql.format(self.primary_key,
-                                                                            self.primary_key,
-                                                                            self.table,
-                                                                            self.primary_key))
-            if len(records) > 0 and len(records[0]) > 0 and records[0][0] > 0:
-                raise ValueError("Data quality check failed. {} has duplicates in {} column"
-                                .format(self.table, self.primary_key))
-            self.log.info(f"Data quality check for primary_key {self.primary_key} " +
-                        f"on table {self.table} passed with no duplicates")
+            self.log.info(f'Test SQL: {sql}')
+            records = redshift_hook.get_records(sql)
+            success = assertion(records)
 
-        # Check that there are no nulls in the not null columns
-        if self.not_nulls != None and len(self.not_nulls) > 0:
-            check_for_nulls_sql = "SELECT count(*) FROM {} WHERE {} is NULL"
-            for col in self.not_nulls:
-                records = redshift_hook.get_records(check_for_nulls_sql.format(self.table, col))
-                if len(records) > 0 and len(records[0]) > 0 and records[0][0] > 0:
-                    num_records = records[0][0]
-                    raise ValueError("Data quality check failed. {} contains {} null(s) in col {}"
-                                    .format(self.table, num_records, col))
-                self.log.info(f"Data quality check passed.  Zero nulls in col {col} on table {self.table}")
+            if not success:
+                raise ValueError(error_message)
+            self.log.info(log_message)
