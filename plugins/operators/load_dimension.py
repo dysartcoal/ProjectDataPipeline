e9fd7585979c4f348c3a3c297976510dee65fb9f
Kim Littlejohn <jkldatascience@gmail.com>
2019-07-16 08:53:13 +0100

    Implement load dimension operator as per spec

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -6,17 +6,43 @@ class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
 
+
+    truncate_dim_sql_template = """
+    TRUNCATE {dest_table}
+    """
+
+    insert_dim_sql_template = """
+    INSERT INTO {dest_table}
+    ({select_sql})
+    """
+
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
-                 *args, **kwargs):
+                redshift_conn_id="",
+                dest_table="",
+                select_sql="",
+                append=True,
+                *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.dest_table=dest_table
+        self.select_sql=select_sql
+        self.append=append
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f'LoadDimensionOperator dest_table: {self.dest_table}, append is {self.append}')
+
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+
+        formatted_truncate_sql = (LoadDimensionOperator
+                                .truncate_dim_sql_template
+                                .format(dest_table=self.dest_table))
+        formatted_insert_sql = (LoadDimensionOperator
+                                .insert_dim_sql_template
+                                .format(dest_table=self.dest_table,
+                                        select_sql=self.select_sql))
+
+        if not self.append:
+            redshift_hook.run(formatted_truncate_sql)
+        redshift_hook.run(formatted_insert_sql)
